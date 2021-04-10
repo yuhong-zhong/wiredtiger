7c07c0b1f08d0e4c8e0d31b17be7d8880d5b06e2
Yuhong Zhong <nanua.zqz@gmail.com>
2021-04-10 19:40:03 -0400

    add debug info

--- src/include/ebpf_traversal.h
+++ src/include/ebpf_traversal.h
@@ -127,12 +127,20 @@ inline int ebpf_parse_cell_addr_int(const uint8_t *cell, uint64_t *offset, uint6
     int ret;
 
     /* verify cell type & validity window & RLE in descriptor byte (1B) */
-    if ((WT_CELL_SHORT_TYPE(cell[0]) != 0)
-        || (WT_CELL_TYPE(cell[0]) != WT_CELL_ADDR_INT)
-        || (cell[0] & WT_CELL_SECOND_DESC != 0)
-        || (cell[0] & WT_CELL_64V != 0)) {
-        return -EBPF_EINVAL;
-    }
+    // if ((WT_CELL_SHORT_TYPE(cell[0]) != 0)
+    //     || (WT_CELL_TYPE(cell[0]) != WT_CELL_ADDR_INT)
+    //     || (cell[0] & WT_CELL_SECOND_DESC != 0)
+    //     || (cell[0] & WT_CELL_64V != 0)) {
+    //     return -EBPF_EINVAL;
+    // }
+    if (WT_CELL_SHORT_TYPE(cell[0]) != 0)
+        return -1;
+    if (WT_CELL_TYPE(cell[0]) != WT_CELL_ADDR_INT)
+        return -2;
+    if (cell[0] & WT_CELL_SECOND_DESC != 0)
+        return -3;
+    if (cell[0] & WT_CELL_64V != 0)
+        return -4;
     p += 1;
 
     /* the cell is followed by data length and a chunk of data */
